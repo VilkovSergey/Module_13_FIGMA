f62c42d1dcbbdc3c9761d48ee98c57bb1d5ee203
VilkovSergey <vilkov75@yandex.ru>
2023-05-16 16:07:16 +0300

    Закончил галлерею и интерактив

--- Работа с Figma и создание проекта/js/main.js
+++ Работа с Figma и создание проекта/js/main.js
@@ -3,5 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances_sidenav = M.Sidenav.init(sidenav);
   var modal = document.querySelectorAll('.modal');
   var instances_modal = M.Modal.init(modal);
+  var gallery = document.querySelectorAll('.materialboxed');
+  var instances_gallery = M.Materialbox.init(gallery);
 });
 
